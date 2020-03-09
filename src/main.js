baafd75cbacfeb239edcb35996711d016266c203
ht62524 <ht625246590@gmail.com>
2020-03-10 04:31:22 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/css/global.css";
+import "./assets/fonts/iconfont.css";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+Vue.prototype.$message = ElementUI.Message;
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,36 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/login.vue";
+import Home from "../components/home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: Login },
+  { path: "/home", component: Home }
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+// 挂在路由守卫对象
+router.beforeEach((to, from, next) => {
+  if (to.path == "/login") {
+    return next();
+  }
+  //获取token
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (!tokenStr) {
+    return next("/login");
+  }
+  next();
+});
+//to将要去的路径
+//from 代表从哪个路径来
+//next 是一个函数，表示放行
+//next() 放行
+//next('/login') 强制跳转到login
+
+export default router;
